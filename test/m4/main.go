7d38f1c31ccc209dd722c2a6665dbeb3058a8b7f
Karel Kubat <karel@kubat.nl>
2022-05-26 19:57:48 +0200

    demo of different checksum lengths

---
+++ test/m4/main.go
@@ -0,0 +1,33 @@
+// file: test/m4/main.go
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/KarelKubat/hrid/conv"
+)
+
+const (
+	alphabet = "ABCDEFGH"
+)
+
+func main() {
+	for checksumLen := 0; checksumLen < 8; checksumLen++ {
+		converter, err := conv.New("ABCDEFGH", checksumLen) // Base-8 conversion, but using funky digits
+		if err != nil {
+			log.Fatal(err)
+		}
+		for nr := uint64(0); nr <= 15; nr++ {
+			id := converter.ToString(nr)
+			decoded, err := converter.ToNr(id)
+			if err != nil {
+				log.Fatal(err)
+			}
+			fmt.Printf("%2v yields ID %12q (with %v checksum digits) which decodes to %v\n", nr, id, checksumLen, decoded)
+			if nr != decoded {
+				log.Fatal("decoding failed")
+			}
+		}
+	}
+}
